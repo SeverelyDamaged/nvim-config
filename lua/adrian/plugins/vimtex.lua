7b8e09a290df56ea5464ac52c0652365118a27af
Adrian Balbalosa <z5397730@ad.unsw.edu.au>
2023-06-08 09:59:36 +1000

    Added skim option for vimtex compile when working on mac

--- lua/adrian/plugins/vimtex.lua
+++ lua/adrian/plugins/vimtex.lua
@@ -1,5 +1,10 @@
-vim.g.vimtex_view_general_viewer = "zathura"
-vim.g.vimtex_view_method = "zathura"
+if vim.loop.os_uname().sysname == "Darwin" then
+	vim.g.vimtex_view_method = "skim"
+	vim.g.vimtex_view_general_viewer = "skim"
+else
+	vim.g.vimtex_view_method = "zathura"
+	vim.g.vimtex_view_general_viewer = "zathura"
+end
 vim.g.tex_comment_nospell = 1
 vim.g.vimtex_compiler_progname = "nvr"
 vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
