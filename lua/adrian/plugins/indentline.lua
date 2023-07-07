1a77b8b28e04225b1c4c3acdd1bff48b7f09c7b8
adrianbalbs <adrianbalbs@hotmail.com>
2023-07-07 18:34:35 +1000

    removed compiled from repo

--- lua/adrian/plugins/indentline.lua
+++ lua/adrian/plugins/indentline.lua
@@ -7,7 +7,7 @@ indent_blankline.setup({
 	char = "▏",
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = true,
-	use_treesitter = true,
+	-- use_treesitter = true,
 	show_current_context = true,
 	buftype_exclude = { "terminal", "nofile" },
 	filetype_exclude = {
