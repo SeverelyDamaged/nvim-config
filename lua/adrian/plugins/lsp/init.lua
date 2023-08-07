e786d5ffbbe60bd105d032aafb8e75dc43fdd025
adrianbalbs <adrianbalbs@hotmail.com>
2023-08-07 10:07:19 +1000

    removed conf

--- lua/adrian/plugins/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require("adrian.plugins.lsp.mason")
-require("adrian.plugins.lsp.handlers").setup()
-require("adrian.plugins.lsp.null-ls")
