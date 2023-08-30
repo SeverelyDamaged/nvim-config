aedb5b39d50c5fb6e910fd5de205a1274cebb4ab
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-08-31 07:34:11 +1000

    Added haskell nvim

---
+++ lua/plugins/haskell-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "mrcjkb/haskell-tools.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim", -- Optional
+  },
+  branch = "2.x.x", -- Recommended
+  ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+}
