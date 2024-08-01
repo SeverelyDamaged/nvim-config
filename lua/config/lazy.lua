ffcf4f1c0aa0909d50652b052a81c7f1ded13b18
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-08-01 11:01:19 +1000

    make sure haskell-tools doesn't get loaded

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/haskell-tools.lua
+++ lua/plugins/haskell-tools.lua
@@ -1,29 +1,36 @@
+local valid_filetypes = { "haskell", "lhaskell", "cabal", "cabalproject" }
+
 return {
   {
     "mrcjkb/haskell-tools.nvim",
+    ft = valid_filetypes,
+    -- TODO: Refactor this to have keys defined somewhere else, causes plugin to be loaded
     keys = function()
-      local ht = require("haskell-tools")
-      local bufnr = vim.api.nvim_get_current_buf()
-      local opts = { noremap = true, silent = true, buffer = bufnr }
-      local icon, _, _ = require("mini.icons").get("extension", "hs")
-      require("which-key").add({
-        "<leader>r",
-        group = "haskell",
-        icon = icon,
-      })
-
-      return {
-        { "<leader>rr", ht.repl.toggle, desc = "Toggle repl", opts },
-        {
-          "<leader>rf",
-          function()
-            ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-          end,
-          desc = "Toggle buffer repl",
-          opts,
-        },
-        { "<leader>rq", ht.repl.quit, desc = "Quit repl", opts },
-      }
+      local filetype = vim.bo.filetype
+      if vim.tbl_contains(valid_filetypes, filetype) then
+        local ht = require("haskell-tools")
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        local icon, _, _ = require("mini.icons").get("extension", "hs")
+        require("which-key").add({
+          "<leader>r",
+          group = "haskell",
+          icon = icon,
+        })
+        return {
+          { "<leader>rr", ht.repl.toggle, desc = "Toggle repl", opts },
+          {
+            "<leader>rf",
+            function()
+              ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+            end,
+            desc = "Toggle buffer repl",
+            opts,
+          },
+          { "<leader>rq", ht.repl.quit, desc = "Quit repl", opts },
+        }
+      end
+      return {}
     end,
   },
 }
