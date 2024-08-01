c30fba251dc88df765bbc3de736515a97edcf64d
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-08-01 11:33:58 +1000

    refactor config

--- lua/plugins/haskell-tools.lua
+++ lua/plugins/haskell-tools.lua
@@ -4,33 +4,22 @@ return {
   {
     "mrcjkb/haskell-tools.nvim",
     ft = valid_filetypes,
-    -- TODO: Refactor this to have keys defined somewhere else, causes plugin to be loaded
-    keys = function()
-      local filetype = vim.bo.filetype
-      if vim.tbl_contains(valid_filetypes, filetype) then
-        local ht = require("haskell-tools")
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        local icon, _, _ = require("mini.icons").get("extension", "hs")
-        require("which-key").add({
-          "<leader>r",
-          group = "haskell",
-          icon = icon,
-        })
-        return {
-          { "<leader>rr", ht.repl.toggle, desc = "Toggle repl", opts },
-          {
-            "<leader>rf",
-            function()
-              ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-            end,
-            desc = "Toggle buffer repl",
-            opts,
-          },
-          { "<leader>rq", ht.repl.quit, desc = "Quit repl", opts },
-        }
-      end
-      return {}
+    opts = function(_, opts)
+      local ht = require("haskell-tools")
+      local bufnr = vim.api.nvim_get_current_buf()
+      local opt = { noremap = true, silent = true, buffer = bufnr }
+      local icon, _, _ = require("mini.icons").get("extension", "hs")
+      require("which-key").add({
+        "<leader>r",
+        group = "haskell",
+        icon = icon,
+      })
+      vim.keymap.set("n", "<leader>rr", ht.repl.toggle, { unpack(opt), desc = "Toggle repl for package" })
+      vim.keymap.set("n", "<leader>rf", function()
+        ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+      end, { unpack(opt), desc = "Toggle repl for buffer" })
+      vim.keymap.set("n", "<leader>rq", ht.repl.quit, { unpack(opt), desc = "Quit repl" })
+      return opts
     end,
   },
 }
